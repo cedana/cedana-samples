d4e79607cff446f559836a8ad97101a0d4f1abbb
Niranjan Ravichandra <nravic@cedana.ai>
2025-07-21 08:30:57 -0400

    fix: separate bindcraft out

---
+++ gpu_smr/compbio/gromacs/build_once.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Run once to build, used for embedding binary inside container
+# This script builds GROMACS from source and installs it. If running on your own machine, should build.
+
+tar xfz gromacs-2025.2.tar.gz
+cd gromacs-2025.2
+mkdir build
+cd build
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON
+make
+make check
+sudo make install
