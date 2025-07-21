a9a01ccd80ecf05e941253f7d5dfa587f20085af
Niranjan Ravichandra <nravic@cedana.ai>
2025-07-21 08:47:25 -0400

    fix: gromacs build

--- gpu_smr/compbio/gromacs/build_once.sh
+++ gpu_smr/compbio/gromacs/build_once.sh
@@ -2,8 +2,7 @@
 
 # Run once to build, used for embedding binary inside container
 # This script builds GROMACS from source and installs it. If running on your own machine, should build.
-wget https://ftp.gromacs.org/gromacs/gromacs-2026.0-dev.tar.gz
-
+wget https://ftp.gromacs.org/gromacs/gromacs-2025.2.tar.gz
 tar xfz gromacs-2025.2.tar.gz
 cd gromacs-2025.2
 mkdir build
